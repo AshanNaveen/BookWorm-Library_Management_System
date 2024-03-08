f012904644461fe533409e641603e14879711e3e
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-03-08 21:08:45 +0530

    Controller | Login Controller Updated

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,4 +1,36 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class LoginFormController {
+    @FXML
+    private JFXTextField txtUsername;
+
+    @FXML
+    private JFXPasswordField txtPasswordHide;
+
+    @FXML
+    private JFXTextField txtPasswordUnHide;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private Text btnSignUp;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(MouseEvent event) {
+
+    }
 }
