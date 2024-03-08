9e58dfd97103049c5ae422bf5fb68951bcacb7ad
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-03-08 22:30:15 +0530

    Session Factory Config Created

--- src/main/java/lk/ijse/AppInitializer.java
+++ src/main/java/lk/ijse/AppInitializer.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lk.ijse.util.SessionFactoryConfig;
 
 public class AppInitializer extends Application {
     public static void main(String[] args) {
---
+++ src/main/java/lk/ijse/util/SessionFactoryConfig.java
@@ -0,0 +1,25 @@
+package lk.ijse.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryConfig {
+    private static SessionFactoryConfig factoryConfig;
+
+    private final SessionFactory sessionFactory;
+
+    private SessionFactoryConfig(){
+        sessionFactory=new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
+
+    public static SessionFactoryConfig getInstance(){
+        return factoryConfig==null ? factoryConfig=new SessionFactoryConfig() : factoryConfig;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+}
