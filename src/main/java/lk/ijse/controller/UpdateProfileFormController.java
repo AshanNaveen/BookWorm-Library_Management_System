f6f0e9779e34d26d832808c90eb53f0594eab02a
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-03-08 21:00:54 +0530

    Controller | Update Profile Controller Updated

--- src/main/java/lk/ijse/controller/UpdateProfileFormController.java
+++ src/main/java/lk/ijse/controller/UpdateProfileFormController.java
@@ -1,4 +1,65 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
 public class UpdateProfileFormController {
+    @FXML
+    private Text btnBackWord;
+
+    @FXML
+    private ImageView btnBackArrow;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtPhone;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtConfirmPassword;
+
+    @FXML
+    private Pane photoPane;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private JFXButton btnUploadPhoto;
+
+    @FXML
+    void btnBackOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUploadPhotoOnAction(ActionEvent event) {
+
+    }
 }
