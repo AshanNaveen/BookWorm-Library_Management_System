5fe3b5a1162c85ee547d2d3858c2f4e8711011cb
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-03-11 18:27:43 +0530

    Util | Navigation util created

---
+++ src/main/java/lk/ijse/util/Navigation.java
@@ -0,0 +1,98 @@
+package lk.ijse.util;
+
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import lk.ijse.controller.FxmlController;
+import lk.ijse.dto.UserDTO;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Navigation {
+    private static Stage stage;
+    private static Scene scene;
+
+    public static void onTheTopNavigation(Pane pane, String link) {
+        try {
+            FXMLLoader loader = new FXMLLoader(Navigation.class.getResource("/view/" + link));
+            Parent root = loader.load();
+            pane.getChildren().add(root);
+            FadeTransition fadeIn = new FadeTransition(Duration.millis(1500), pane);
+            fadeIn.setFromValue(0.0);
+            fadeIn.setToValue(1.0);
+            fadeIn.play();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void switchNavigation(String link, ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(Navigation.class.getResource("/view/" + link));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.centerOnScreen();
+            stage.show();
+            FadeTransition fadeIn = new FadeTransition(Duration.millis(800), scene.getRoot());
+            fadeIn.setFromValue(0.0);
+            fadeIn.setToValue(1.0);
+            fadeIn.play();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void switchNavigation(String link, MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Navigation.class.getResource("/view/" + link));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+        FadeTransition fadeIn = new FadeTransition(Duration.millis(800), scene.getRoot());
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
+    }
+
+    public static void popupNavigation(String link,String title){
+        try {
+            URL resource = Navigation.class.getResource("/view/" + link);
+            Parent parent = FXMLLoader.load(resource);
+            Scene scene = new Scene(parent);
+            Stage stage = new Stage();
+            stage.setAlwaysOnTop(true);
+            stage.setTitle(title);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(ActionEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        stage = (Stage) node.getScene().getWindow();
+        stage.close();
+    }
+
+    public static void close(MouseEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        stage = (Stage) node.getScene().getWindow();
+        stage.close();
+    }
+
+    public static void close(Pane pane) {
+        pane.getChildren().clear();
+    }
+}
