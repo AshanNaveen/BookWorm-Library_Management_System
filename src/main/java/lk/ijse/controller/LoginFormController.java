33fdd3dbe1067cbcdc9a645b6378e01bc1e59d93
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-03-11 18:30:41 +0530

    Controller | login-form Controller Updated

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -3,13 +3,24 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import jakarta.persistence.NoResultException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import lk.ijse.bo.UserBO;
 import lk.ijse.bo.UserBOImpl;
 import lk.ijse.dto.UserDTO;
+import lk.ijse.util.Navigation;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class LoginFormController {
     @FXML
@@ -27,36 +38,38 @@ public class LoginFormController {
     @FXML
     private Text btnSignUp;
 
+    @FXML
+    private Label lblWarning;
+
+    public static Long userID;
+
     UserBO userBO = new UserBOImpl();
 
     @FXML
     void btnLoginOnAction(ActionEvent event) {
-        UserDTO userDTO = userBO.findCredential(txtUsername.getText());
-        System.out.println(userDTO);
+        try {
+            UserDTO userDTO = userBO.findCredential(txtUsername.getText());
+            System.out.println(userDTO);
 
-        if (userDTO!=null){
-            if (userDTO.getGmail()!=null && textPassword.getText().equals(userDTO.getPassword())){
-                Parent root = null;
-                root = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/dashbord-form.fxml")));
-                if (root != null) {
-                    Scene subScene = new Scene(root);
-                    Stage primaryStage = (Stage) this.root.getScene().getWindow();
-                    primaryStage.setScene(subScene);
-                    primaryStage.centerOnScreen();
-
-                    TranslateTransition tt = new TranslateTransition(Duration.millis(350), subScene.getRoot());
-                    tt.setFromX(-subScene.getWidth());
-                    tt.setToX(0);
-                    tt.play();
+            if (userDTO != null) {
+                if (userDTO.getEmail() != null && txtPasswordHide.getText().equals(userDTO.getPassword())) {
+                    userID=userDTO.getId();
+                    Navigation.switchNavigation("user-dashboard-form.fxml", event);
                 }
+            } else {
+                lblWarning.setText("Invalid Username or Password !");
             }
-        }else {
-            lblWarning.setText("empty value or Invalid UserNAme or Password !");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     @FXML
     void btnSignUpOnAction(MouseEvent event) {
-
+        try {
+            Navigation.switchNavigation("sign-up-form.fxml", event);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
