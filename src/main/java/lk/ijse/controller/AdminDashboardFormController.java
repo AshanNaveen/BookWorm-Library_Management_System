979d9f1599de60816a71fdd6602932db1469fa80
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-03-15 22:39:34 +0530

    Controller | Admin Dashboard Controller Created

---
+++ src/main/java/lk/ijse/controller/AdminDashboardFormController.java
@@ -0,0 +1,37 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import lk.ijse.util.Navigation;
+
+public class AdminDashboardFormController {
+    @FXML
+    private JFXButton btnLogOut;
+
+    @FXML
+    void btnAdminOnAction(MouseEvent event) {
+        Navigation.switchNavigation("admins-mangement-form.fxml",event);
+    }
+
+    @FXML
+    void btnBookOnAction(MouseEvent event) {
+        Navigation.switchNavigation("books-mangement-form.fxml",event);
+    }
+
+    @FXML
+    void btnBranchOnAction(MouseEvent event) {
+        Navigation.switchNavigation("branches-mangement-form.fxml",event);
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+        Navigation.switchNavigation("login-form.fxml",event);
+    }
+
+    @FXML
+    void btnUsersNotReturnedOnAction(MouseEvent event) {
+        Navigation.switchNavigation("usersNotReturend-form.fxml",event);
+    }
+}
