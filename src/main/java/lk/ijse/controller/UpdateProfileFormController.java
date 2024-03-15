a22fabe59ce7b1473b6c0cb676e1ccfa457fc3d4
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-03-15 22:52:39 +0530

    Controller | Update Profile Controller Created

--- src/main/java/lk/ijse/controller/UpdateProfileFormController.java
+++ src/main/java/lk/ijse/controller/UpdateProfileFormController.java
@@ -4,10 +4,23 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import lk.ijse.bo.UserBO;
+import lk.ijse.bo.UserBOImpl;
+import lk.ijse.dto.UserDTO;
+import lk.ijse.util.Navigation;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Optional;
 
 public class UpdateProfileFormController {
     @FXML
@@ -42,15 +55,24 @@ public class UpdateProfileFormController {
 
     @FXML
     private JFXButton btnUploadPhoto;
+    private UserBO userBO=new UserBOImpl();
 
     @FXML
-    void btnBackOnAction(MouseEvent event) {
-
+    void btnBackOnAction(MouseEvent event) throws IOException {
+        Navigation.switchNavigation("user-dashboard-form.fxml",event);
     }
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
+        ButtonType yes = new ButtonType("yes", ButtonBar.ButtonData.OK_DONE);
+        ButtonType no = new ButtonType("no", ButtonBar.ButtonData.CANCEL_CLOSE);
 
+        Optional<ButtonType> type = new Alert(Alert.AlertType.INFORMATION, "Are you sure to remove?", yes, no).showAndWait();
+
+        if (type.orElse(no) == yes) {
+            userBO.deleteUser(LoginFormController.userID);
+        }
+        Navigation.switchNavigation("login-form.fxml",event);
     }
 
     @FXML
@@ -60,6 +82,14 @@ public class UpdateProfileFormController {
 
     @FXML
     void btnUploadPhotoOnAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files","*.png","*.jpg","*.jpeg","*.gif"));
 
+        File selectedFile = fileChooser.showOpenDialog(btnUploadPhoto.getScene().getWindow());
+
+        if (selectedFile !=null) {
+
+        }
     }
 }
