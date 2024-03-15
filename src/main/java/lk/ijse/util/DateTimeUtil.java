3ca7da905a9cf744e2dc7655c9a2b5daf4863b7c
Ashan Naveen Theekshana <ashannvn@gmail.com>
2024-03-15 22:47:36 +0530

    Util Update

--- src/main/java/lk/ijse/util/DateTimeUtil.java
+++ src/main/java/lk/ijse/util/DateTimeUtil.java
@@ -1,14 +1,31 @@
 package lk.ijse.util;
 
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateTimeUtil {
+    public static Timestamp getDueTimestamp(Timestamp timestamp) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timestamp.getTime());
+
+        calendar.add(Calendar.WEEK_OF_YEAR, 2);
+
+        Timestamp newTimestamp = new Timestamp(calendar.getTimeInMillis());
+        return newTimestamp;
+    }
     public static String dateNow() {
         SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy");
         return format.format(new Date());
+    }
+
+    public static String convertDate(Timestamp timestamp) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy / MM / dd");
+        Date date = new Date(timestamp.getTime());
+        return sdf.format(date);
     }
 
     public static String yearNow() {
@@ -19,7 +36,6 @@ public class DateTimeUtil {
     public static int getDays(int year, int month) {
         YearMonth yearMonthObject = YearMonth.of(year, month);
         return yearMonthObject.lengthOfMonth();
-
     }
 
     public static String monthNow() {
